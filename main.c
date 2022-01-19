15f05781a7f9c3d9b3327cfe667819566d918d42
keigo_shimada <e215730@ie.u-ryukyu.ac.jp>
2022-01-19 16:11:36 +0900

    :+1: `Clean` main.c
    実行例2の追加．

--- main.c
+++ main.c
@@ -10,13 +10,25 @@ void selection_sort_array(int A[], int N);
 void print_array(int A[], int B[], int N);
 
 int main(void) {
+    // 実行例1
     const int SIZE = 5;
     int scores[SIZE] = {0, 60, 70, 100, 90};
     int results[SIZE];
     memcpy(results, scores, sizeof(scores));
 
+    printf("実行例1\n");
     selection_sort_array(results, SIZE);
     print_array(scores, results, SIZE);
+
+    // 実行例2
+    const int SIZE2 = 6;
+    int scores2[SIZE2] = {100, 60, 70, 100, 90, 80};
+    int results2[SIZE2];
+    memcpy(results2, scores2, sizeof(scores2));
+
+    printf("\n実行例2\n");
+    selection_sort_array(results2, SIZE);
+    print_array(scores2, results2, SIZE);
 
     return 0;
 }
